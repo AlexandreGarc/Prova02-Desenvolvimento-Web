12a70b2f50202cd70a8ed342fa083a6ff13e6994
AlexandreGarcia <89819010+AlexandreGarc@users.noreply.github.com>
2022-05-17 22:23:54 -0300

    Add files via upload

---
+++ prova2_Cadastro_de_produtos/Cadastro de Produtos/footer.php
@@ -0,0 +1,10 @@
+<html>
+	<body>
+	<footer class="rodape">
+		<p>Fatec Araras - Alexandre &copy; 2022</p>
+	</footer>
+
+	<script src="assets/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
